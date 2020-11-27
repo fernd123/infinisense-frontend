83b72741afd506dcae09b3176226818c7a387c70
fernd123 <frodriguezsidro@gmail.com>
2020-11-27 13:46:09 +0100

    engines node changed 2

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const path = require('path');
+const path = require('path')
 const app = express();
 
-app.use(express.static(__dirname + '/dist/infinisense-frontend'));
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/dist/infinisense-frontend/index.html'));
+/*
+app.use(express.static(path.resolve(__dirname, 'dist/dcapp')));
+app.set('port', process.env.PORT || 3000);
+*/
+/*
+app.listen(app.get('port'), function() {
+ console.log('listening to Port', app.get('port'));
 });
-app.listen(process.env.PORT || 8080);
+*/
+
+app.use(express.static('dist/infinisense-frontend'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/infinisense-frontend/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server started');
+})

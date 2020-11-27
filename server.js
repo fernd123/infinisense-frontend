379f024b993044f627f7571a955268fd454edced
fernd123 <frodriguezsidro@gmail.com>
2020-11-27 15:16:09 +0100

    e

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-app.use(express.static('dist/dcapp'));
+app.use(express.static('dist/infinisense-frontend'));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/dcapp/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/infinisense-frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
